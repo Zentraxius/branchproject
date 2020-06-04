52b5de7417e7fa95e435573275152acc5d456ad8
Cody Fritz <clanalia55@gmail.com>
2020-06-04 11:51:12 -0700

    add commit to save progress
    co-authored-by: Taylor Somers <tsomers@yandex.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,24 @@
-$(document).ready(function () {
-  $("buttonplaceholderpleasereplacethis").click(function () {
-    $("bodyisusedasanexample").removeclass("removed-class, alsothis");
-    $("h2isusedasanexample").addclass("iwanttoaddthis, andthis");
-  });
-});
+// $(document).ready(function () {
+//   $("buttonplaceholderpleasereplacethis").click(function () {
+//     $("bodyisusedasanexample").removeclass("removed-class, alsothis");
+//     $("h2isusedasanexample").addclass("iwanttoaddthis, andthis");
+//   });
+// });
+
+// $(document).ready(function () {
+//   $("#cat-hidden").click(function () {
+//     $(".cat-showing").slideToggle();
+//     $("#cat-hidden").slideToggle();
+//   });
+//   $("#testid2").click(function () {
+//     $("#cat-hidden").slideToggle();
+//     $(".cat-showing").slideToggle();
+//   });
+// });
 
 $(document).ready(function () {
-  $(".card-body").click(function () {
-    $(this).slideToggle();
-    $(this).slideToggle();
+  $("button").click(function () {
+    $(".card-body").slideToggle();
+    $(".card-body").slideToggle();
   });
 });
